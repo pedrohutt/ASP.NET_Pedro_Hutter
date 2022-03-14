c20e0c2b7e892b856ce1e9bfd2eb7751a9e7d54c
Pedro Hutter <74618767+pedrohutt@users.noreply.github.com>
2022-03-14 16:32:30 -0300

    Delete TP3/TeamManager/TeamManager directory

--- TP3/TeamManager/TeamManager/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TeamManager.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
