dfb7e6a2d37aaaa322e998300b523095097a1687
Pedro Paulo Barillo Taboada Hutter <pedro.hutter@al.infnet.edu.br>
2022-03-28 18:06:49 -0300

    up

---
+++ TP4/TP4/TP4/TP4/Models/Player.cs
@@ -0,0 +1,9 @@
+﻿namespace TP4.Models
+{
+    public class Player
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Age { get; set; }
+    }
+}
