f24457ca6ca4b2a167027c1e8940c887a3f70e92
Pedro Hutter <74618767+pedrohutt@users.noreply.github.com>
2022-04-06 16:27:13 -0300

    Delete AT/AT directory

--- AT/AT/AT/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AT.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
